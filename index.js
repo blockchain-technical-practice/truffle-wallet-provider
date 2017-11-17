3bf41cd96cee4b8fa80fbb9f3cdd322f1acedb17
gcc2ge <1414786333@qq.com>
2017-11-17 12:15:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var bip39 = require("bip39");
+var Wallet = require('ethereumjs-wallet');
+var ProviderEngine = require("web3-provider-engine");
+var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
+var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+var Web3 = require("web3");
+
+function WalletProvider(privateKey) {
+  this.mnemonic = mnemonic;
+
+  if (address_index == null) {
+    address_index = 0;
+  }
+
+  this.wallet = Wallet.fromPrivateKey(privateKey);
+  this.address = this.wallet.getAddressString();
+
+  this.engine = new ProviderEngine();
+  this.engine.addProvider(new WalletSubprovider(this.wallet, {}));
+  this.engine.addProvider(new FiltersSubprovider());
+  this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
+  this.engine.start(); // Required by the provider engine.
+};
+
+WalletProvider.prototype.sendAsync = function() {
+  this.engine.sendAsync.apply(this.engine, arguments);
+};
+
+WalletProvider.prototype.send = function() {
+  return this.engine.send.apply(this.engine, arguments);
+};
+
+WalletProvider.prototype.getAddress = function() {
+  return this.address;
+};
+
+module.exports = WalletProvider;
