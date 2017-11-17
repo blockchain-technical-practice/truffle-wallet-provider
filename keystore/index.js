21a29c5fb61d6f8a92a437cbceec6d4be4601790
gcc2ge <1414786333@qq.com>
2017-11-17 14:53:26 +0800

    init

--- keystore/index.js
+++ keystore/index.js
@@ -1,5 +1,7 @@
 var keythereum = require("keythereum");
 
+/*
+//async method
 function getPrivateFromkeystore(address,password, datadir){
     return new Promise(function (resolve, reject) {
         keythereum.importFromFile(address, datadir, resolve)
@@ -8,6 +10,14 @@ function getPrivateFromkeystore(address,password, datadir){
             keythereum.recover(password, keyObject, resolve)
         })
     })
+}
+*/
+
+
+function getPrivateFromkeystore(address,password, datadir){
+    var keyObject=keythereum.importFromFile(address, datadir);
+    var privatekey=keythereum.recover(password, keyObject);
+    return '0x' + privatekey.toString('hex');
 }
 
 module.exports.getPrivateFromkeystore = getPrivateFromkeystore;
